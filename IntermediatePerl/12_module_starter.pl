5689a0c35ad57d6a17602ebb33cc16e06e49bc0d
Pano Papadatos <ppapadatos@sourcefire.com>
2013-10-08 12:05:49 -0400

    12

---
+++ IntermediatePerl/12_module_starter.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $whatIDidWas = <<'THISTHING'; #Heredoc with single quotes means interpolation goes away
+
+=================================
+What I did was:
+=================================
+ 
+sudo cpan Module::Starter;
+mkdir ~/.module-starter/;
+echo -e "Author: Pano Papadatos\nemail: pano@heypano.com\nbuilder: Module::Build\nverbose: 1" > ~/.module-starter/config;
+cd ~/checkouts/LearningPerl/IntermediatePerl/;
+mkdir 12_module;
+cd 12_module;
+module-starter -module=Animal;
+cd Animal;
+perl Build.PL;
+./Build;
+./Build test;
+./Build disttest;
+./Build dist;
+THISTHING
+
+print $whatIDidWas; 
